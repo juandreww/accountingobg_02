2ac066ce94fbfeb396fba65fd95205489d4b3670
juandreww <juandreww@outlook.com>
2022-04-07 06:49:16 +0700

    pilihlah aku

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -68,9 +68,9 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd4ehughfapgq0k'),
+            'username' => env('DB_USERNAME', 'awtpbzyctlpydm'),
+            'password' => env('DB_PASSWORD', 'a7bf40c39496f73a03e7412befbc787d29138445d7fce2a34bf31df40cf07d96'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
