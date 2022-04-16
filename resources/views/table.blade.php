4393683cf624a5fc2c7341bce6e823d5966adb0e
odessaobg <103197917+odessaobg@users.noreply.github.com>
2022-04-16 22:25:31 +0700

    dep

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -1,36 +1,35 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="/css/style.css">
-        {{-- <script type="text/javascript" src="/js/app.js"></script> --}}
-
+        <script type="text/javascript" src="/js/app.js"></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <title>My Tasks</title>
     </head>
     <body>
+        <div class="container-fluid">
+            
+        </div>
         <div class="container">
             <div class="card">
                 <div class="card-body">
-                    <div class="table table-bordered">
-                        <thead>
+                    <table class="table table-bordered">
+                        <tr>
+                            <th>Name</td>
+                            <th>Email</td>
+                        </tr>
+                        @foreach ($user as $r)
                             <tr>
-                                <th>Name</td>
-                                <th>Email</td>
+                                <td>{{$r->name}}</td>
+                                <td>{{$r->email}}</td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($user as $r)
-                                <tr>
-                                    <td>{{$r->name}}</td>
-                                    <td>{{$r->email}}</td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </div>
+                        @endforeach
+                    </table>
                 </div>
             </div>
         </div>
-
     </body>
 </html>
