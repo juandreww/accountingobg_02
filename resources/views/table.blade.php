23854e546da45e09ae859ca980fd1409943a471c
juandreww <juandreww@outlook.com>
2022-04-16 18:31:42 +0700

    coommmittt

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -1,29 +1,36 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="{{ asset('/css/app.css') }}">
-        <script type="text/javascript" src="/js/app.js"></script>
+        <link rel="stylesheet" type="text/css" href="/css/style.css">
+        {{-- <script type="text/javascript" src="/js/app.js"></script> --}}
 
         <title>My Tasks</title>
     </head>
     <body>
-        <div class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>Name</td>
-                    <th>Email</td>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($user as $r)
-                    <tr>
-                        <td>{{$r->name}}</td>
-                        <td>{{$r->email}}</td>
-                    </tr>
-                @endforeach
-            </tbody>
+        <div class="container">
+            <div class="card">
+                <div class="card-body">
+                    <div class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Name</td>
+                                <th>Email</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($user as $r)
+                                <tr>
+                                    <td>{{$r->name}}</td>
+                                    <td>{{$r->email}}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </div>
+                </div>
+            </div>
         </div>
+
     </body>
 </html>
