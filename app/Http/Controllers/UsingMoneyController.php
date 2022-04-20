cdd9fd38c048f667f2367cf7109cec9282d6a7f6
juandreww <juandreww@outlook.com>
2022-04-20 21:51:51 +0700

    usingmoneycontroller

--- app/Http/Controllers/UsingMoneyController.php
+++ app/Http/Controllers/UsingMoneyController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use \App\Models\User;
+use \App\Models\UsingMoney;
 
 class UsingMoneyController extends Controller
 {
     public function list(Request $request) {
-
+        $data = UsingMoney::get();
+        return "success";
     }
 }
