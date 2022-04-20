6528aa3879d16838bb4bad4b7515c7fa012de753
juandreww <juandreww@outlook.com>
2022-04-20 21:53:18 +0700

    list blade

---
+++ resources/views/usingmoney/list.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="/css/style.css">
+        <script type="text/javascript" src="/js/app.js"></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <title>UsingMoney</title>
+        <style>
+
+        </style>
+    </head>
+    <body>
+
+    </body>
+</html>
