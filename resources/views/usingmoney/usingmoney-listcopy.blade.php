f4e94d81a2368ecddd8c87fc9b1edf8f9201aba4
juandreww <juandreww@outlook.com>
2022-04-25 06:07:17 +0700

    ajax jquery

--- resources/views/usingmoney/usingmoney-listcopy.blade.php
+++ resources/views/usingmoney/usingmoney-listcopy.blade.php
@@ -10,10 +10,17 @@
         });
     </script> --}}
 
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link  href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" rel="stylesheet">
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    {{-- <script src="{{ asset('js/app.js') }}" defer></script>
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
 
     <title>Document</title>
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> --}}
@@ -21,7 +28,7 @@
 </head>
 <body>
     <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" id="btn">
+    {{-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" id="btn">
         Launch demo modal
     </button>
 
@@ -44,7 +51,7 @@
             </div>
         </div>
         </div>
-    </div>
+    </div> --}}
 
     {{-- <script type="text/javascript">
         jQuery(function($) {
@@ -59,172 +66,92 @@
     {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script> --}}
     {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> --}}
     {{-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script> --}}
-    @extends('layouts.app')
-
-    @section('content')
-
-        <div class="row">
-            <div class="col-lg-12 margin-tb">
-                <div class="pull-left">
-                    <h2>Laravel 8 CRUD </h2>
-                </div>
-                <div class="pull-right">
-                    <a class="btn btn-success text-light" data-toggle="modal" id="mediumButton" data-target="#mediumModal"
-                        data-attr="{{ route('projects.create') }}" title="Create a project"> <i class="fas fa-plus-circle"></i>
-                    </a>
-                </div>
-            </div>
-        </div>
-
-        @if ($message = Session::get('success'))
-            <div class="alert alert-success">
-                <p>{{ $message }}</p>
-            </div>
-        @endif
-
-        <table class="table table-bordered table-responsive-lg table-hover">
-            <thead class="thead-dark">
-                <tr>
-                    <th scope="col">No</th>
-                    <th scope="col">Name</th>
-                    <th scope="col" width="30%">Introduction</th>
-                    <th scope="col">Location</th>
-                    <th scope="col">Cost</th>
-                    <th scope="col">Date Created</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($projects as $project)
-                    <tr>
-                        <td scope="row">{{ ++$i }}</td>
-                        <td>{{ $project->name }}</td>
-                        <td>{{ $project->introduction }}</td>
-                        <td>{{ $project->location }}</td>
-                        <td>{{ $project->cost }}</td>
-                        <td>{{ date_format($project->created_at, 'jS M Y') }}</td>
-                        <td>
-                            <form action="{{ route('projects.destroy', $project->id) }}" method="POST">
-
-                                <a data-toggle="modal" id="smallButton" data-target="#smallModal"
-                                    data-attr="{{ route('projects.show', $project->id) }}" title="show">
-                                    <i class="fas fa-eye text-success  fa-lg"></i>
-                                </a>
-
-                                <a class="text-secondary" data-toggle="modal" id="mediumButton" data-target="#mediumModal"
-                                    data-attr="{{ route('projects.edit', $project->id) }}">
-                                    <i class="fas fa-edit text-gray-300"></i>
-                                </a>
-                                @csrf
-                                @method('DELETE')
-
-                                <button type="submit" title="delete" style="border: none; background-color:transparent;">
-                                    <i class="fas fa-trash fa-lg text-danger"></i>
-                                </button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-
-        {!! $projects->links() !!}
-
-
-        <!-- small modal -->
-        <div class="modal fade" id="smallModal" tabindex="-1" role="dialog" aria-labelledby="smallModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog modal-sm" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body" id="smallBody">
-                        <div>
-                            <!-- the result to be displayed apply here -->
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-        <!-- medium modal -->
-        <div class="modal fade" id="mediumModal" tabindex="-1" role="dialog" aria-labelledby="mediumModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body" id="mediumBody">
-                        <div>
-                            <!-- the result to be displayed apply here -->
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-        <script>
-            // display a modal (small modal)
-            $(document).on('click', '#smallButton', function(event) {
-                event.preventDefault();
-                let href = $(this).attr('data-attr');
-                $.ajax({
-                    url: href,
-                    beforeSend: function() {
-                        $('#loader').show();
-                    },
-                    // return the result
-                    success: function(result) {
-                        $('#smallModal').modal("show");
-                        $('#smallBody').html(result).show();
-                    },
-                    complete: function() {
-                        $('#loader').hide();
-                    },
-                    error: function(jqXHR, testStatus, error) {
-                        console.log(error);
-                        alert("Page " + href + " cannot open. Error:" + error);
-                        $('#loader').hide();
-                    },
-                    timeout: 8000
-                })
-            });
-
-            // display a modal (medium modal)
-            $(document).on('click', '#mediumButton', function(event) {
-                event.preventDefault();
-                let href = $(this).attr('data-attr');
-                $.ajax({
-                    url: href,
-                    beforeSend: function() {
-                        $('#loader').show();
-                    },
-                    // return the result
-                    success: function(result) {
-                        $('#mediumModal').modal("show");
-                        $('#mediumBody').html(result).show();
-                    },
-                    complete: function() {
-                        $('#loader').hide();
-                    },
-                    error: function(jqXHR, testStatus, error) {
-                        console.log(error);
-                        alert("Page " + href + " cannot open. Error:" + error);
-                        $('#loader').hide();
-                    },
-                    timeout: 8000
-                })
-            });
-
-        </script>
-    @endsection
+    <a class="btn btn-success" onClick="add()" href="javascript:void(0)"> Create Company</a>
 </body>
+<script type="text/javascript">
+$(document).ready( function () {
+    $.ajaxSetup({
+        headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+    $('#ajax-crud-datatable').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: "{{ url('ajax-crud-datatable') }}",
+        columns: [
+        { data: 'id', name: 'id' },
+        { data: 'name', name: 'name' },
+        { data: 'email', name: 'email' },
+        { data: 'address', name: 'address' },
+        { data: 'created_at', name: 'created_at' },
+        {data: 'action', name: 'action', orderable: false},
+        ],
+        order: [[0, 'desc']]
+    });
+});
+
+function add(){
+    $('#CompanyForm').trigger("reset");
+    $('#CompanyModal').html("Add Company");
+    $('#company-modal').modal('show');
+    $('#id').val('');
+}
+function editFunc(id){
+    $.ajax({
+        type:"POST",
+        url: "{{ url('edit-company') }}",
+        data: { id: id },
+        dataType: 'json',
+        success: function(res){
+            $('#CompanyModal').html("Edit Company");
+            $('#company-modal').modal('show');
+            $('#id').val(res.id);
+            $('#name').val(res.name);
+            $('#address').val(res.address);
+            $('#email').val(res.email);
+        }
+    });
+}
+
+function deleteFunc(id){
+    if (confirm("Delete Record?") == true) {
+        var id = id;
+        // ajax
+        $.ajax({
+            type:"POST",
+            url: "{{ url('delete-company') }}",
+            data: { id: id },
+            dataType: 'json',
+            success: function(res){
+                var oTable = $('#ajax-crud-datatable').dataTable();
+                oTable.fnDraw(false);
+            }
+        });
+    }
+}
+
+$('#CompanyForm').submit(function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    $.ajax({
+        type:'POST',
+        url: "{{ url('store-company')}}",
+        data: formData,
+        cache:false,
+        contentType: false,
+        processData: false,
+        success: (data) => {
+            $("#company-modal").modal('hide');
+            var oTable = $('#ajax-crud-datatable').dataTable();
+            oTable.fnDraw(false);
+            $("#btn-save").html('Submit');
+            $("#btn-save"). attr("disabled", false);
+        },
+        error: function(data) {
+            console.log(data);
+        }
+    });
+});
+</script>
 </html>
