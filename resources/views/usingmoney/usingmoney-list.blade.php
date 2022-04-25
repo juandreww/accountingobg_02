12475811848aa742f1b8a5c267bd7f8525ed3a44
juandreww <juandreww@outlook.com>
2022-04-26 05:54:15 +0700

    using money list

--- resources/views/usingmoney/usingmoney-list.blade.php
+++ resources/views/usingmoney/usingmoney-list.blade.php
@@ -3,21 +3,6 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        {{-- <link rel="stylesheet" type="text/css" href="/css/style.css"> --}}
-        {{-- <script type="text/javascript" src="/js/app.js"></script> --}}
-        {{-- <script type="text/javascript">
-            $('#exampleModal').on('show.bs.modal', function (event) {
-            var button = $(event.relatedTarget) // Button that triggered the modal
-            var recipient = button.data('whatever') // Extract info from data-* attributes
-            // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-            // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-            var modal = $(this)
-            modal.find('.modal-title').text('New message to ' + recipient)
-            modal.find('.modal-body input').val(recipient)
-            })
-        </script> --}}
-        {{-- <script src="{{ asset('js/app.js') }}" defer></script>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
         <title>UsingMoney</title>
         <style>
 
@@ -119,22 +104,6 @@
             </div>
           </div>
         </div>
-
-        <form action="/usingmoney/formtransaction" method="GET" class="form-inline">
-            <input class="btn btn-primary ml-3" type="submit" value="CARI">
-        </form>
-
-        <a href ="#" data-toggle="modal" data-target="#modalfade">Open Modal</a>
-
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalfade" id="btn">
-            Launch demo modal
-        </button>
-
-        <script>
-            $('#modalfade').on('show.bs.modal', function (e) {
-            // do something...
-            })
-        </script>
 
     </body>
 </html>
