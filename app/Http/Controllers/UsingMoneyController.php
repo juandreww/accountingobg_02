70660a569c1ff6be642b9892376d9f21219f3814
juandreww <juandreww@outlook.com>
2022-05-12 21:38:03 +0700

    db select query bindings

--- app/Http/Controllers/UsingMoneyController.php
+++ app/Http/Controllers/UsingMoneyController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use \App\Models\User;
 use \App\Models\UsingMoney;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 class UsingMoneyController extends Controller
 {
@@ -14,12 +15,19 @@ class UsingMoneyController extends Controller
         $now = now()->addHours(7)->firstOfMonth()->toDateTimeString();
         // $data = UsingMoney::where('date','>=',$now)->get();
         $data = UsingMoney::orderBy('date')->get();
+        $b = $data[0];
+        $n = []; $date = '2022-05-03';
+        $c = DB::select("SELECT * FROM trnusingmoney WHERE Date= :date LIMIT 1", ['date' => $date]);
+        dd($c);
+        DB::transaction(function (&$n, $b) {
+            $n = $b;
+        });
+        dd($n);
         // return view('usingmoney.usingmoney-listcopy');
         return view('usingmoney.usingmoney-list', ['data' => $data]);
     }
 
     public function formtransaction(Request $request) {
-
         $data = UsingMoney::get();
         // return view('usingmoney.usingmoney-listcopy');
         return view('usingmoney.usingmoney-formtransaction');
