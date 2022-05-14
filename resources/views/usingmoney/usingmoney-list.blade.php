da65f60b8259b148a94cdb2a9bdb9dd544721922
juandreww <juandreww@outlook.com>
2022-05-14 22:50:41 +0700

    usingmoney list

--- resources/views/usingmoney/usingmoney-list.blade.php
+++ resources/views/usingmoney/usingmoney-list.blade.php
@@ -211,6 +211,8 @@
             </div>
         </div>
 
+        <a href="/usingmoney/exportexcel" class="btn btn-success my-3" target="_blank">EXPORT EXCEL</a>
+
         <script type="text/javascript" src="{{ asset('/js/usingmoney.js') }}">
         </script>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,4 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/usingmoney/list', [App\Http\Controllers\UsingMoneyController::class, 'list']);
 Route::get('/usingmoney/formtransaction', [App\Http\Controllers\UsingMoneyController::class, 'formtransaction']);
 Route::post('/usingmoney/savetransaction', [App\Http\Controllers\UsingMoneyController::class, 'savetransaction']);
+Route::get('/usingmoney/exportexcel', [App\Http\Controllers\UsingMoneyController::class, 'export_excel']);
