1aa4c66e8c96b194ceef7c211ef3938ebe9bdc4c
juandreww <juandreww@outlook.com>
2022-05-15 11:58:11 +0700

    export with format

--- app/Exports/UsingMoneyExport.php
+++ app/Exports/UsingMoneyExport.php
@@ -7,26 +7,24 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\Exportable;
-use App\Exports\Date;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Concerns\RegistersEventListeners;
+use Maatwebsite\Excel\Events\BeforeExport;
+use Maatwebsite\Excel\Events\BeforeWriting;
+use Maatwebsite\Excel\Events\BeforeSheet;
+use Maatwebsite\Excel\Events\AfterSheet;
 
+Sheet::macro('styleCells', function (Sheet $sheet, string $cellRange, array $style) {
+    $sheet->getDelegate()->getStyle($cellRange)->applyFromArray($style);
+});
 
-class UsingMoneyExport implements FromQuery, WithMapping
+class UsingMoneyExport implements FromQuery, WithMapping, WithHeadings, WithEvents
 {
-    use Exportable;
-
-    // public function collection()
-    // {
-    //     // $data = UsingMoney::get();
-    //     $result = [];
-    //     // foreach ($data as $d) {
-    //     //     $result[] = [
-    //     //         'date' => $d->date,
-    //     //         'amount'=> $d->amount,
-    //     //         'note' => $d->note
-    //     //     ];
-    //     // }
-    //     return $result;
-    // }
+    use Exportable, RegistersEventListeners;
 
     public function query()
     {
@@ -37,8 +35,55 @@ class UsingMoneyExport implements FromQuery, WithMapping
     {
         return [
             $data->amount,
-            Date::dateTimeToExcel($data->date),
+            $data->date,
             $data->note,
+        ];
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Amount',
+            'Date',
+            'Note',
+        ];
+    }
+
+    public static function beforeExport(BeforeExport $event)
+    {
+        //
+    }
+
+    public static function beforeWriting(BeforeWriting $event)
+    {
+        //
+    }
+
+    public static function beforeSheet(BeforeSheet $event)
+    {
+        //
+    }
+
+    public static function afterSheet(AfterSheet $event)
+    {
+        $event->sheet->styleCells(
+            'A1',
+            [
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                ],
+                'fill' => [
+                    'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                    'color' => ['argb' => '#F7948E']
+                ]
+            ]
+        );
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => [self::class, 'afterSheet']
         ];
     }
 }
