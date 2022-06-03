2c70bbe888e6dc3ce1305a408828440eecc3d085
juandreww <juandreww@outlook.com>
2022-06-03 22:04:20 +0700

    icon cute

--- resources/views/usingmoney/report.blade.php
+++ resources/views/usingmoney/report.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="_token" content="{{csrf_token()}}" />
     <title>Report</title>
     <style>
 
@@ -12,6 +13,14 @@
     <link rel="stylesheet" href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 <body>
+    <div class="sidenav">
+        <span class="material-symbols-outlined md-36 sidenav-button">menu</span>
+        <span class="material-symbols-outlined md-36 sidenav-button">house</span>
+        <span class="material-symbols-outlined md-36 sidenav-button">receipt_long</span>
+        <span class="material-symbols-outlined md-36 sidenav-button">account_balance_wallet</span>
+        <span class="material-symbols-outlined md-36 sidenav-button">logout</span>
+    </div>
+    <div class="content"></div>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
