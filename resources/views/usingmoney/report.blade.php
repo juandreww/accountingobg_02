b9a483bc059b701daf4ad2aee45c0a32006db5fc
juandreww <juandreww@outlook.com>
2022-06-03 22:50:02 +0700

    report

--- resources/views/usingmoney/report.blade.php
+++ resources/views/usingmoney/report.blade.php
@@ -48,6 +48,10 @@
             color:#94849C;
         }
 
+        .sidenav li a:hover {
+            color: #d66565;
+        }
+
 
     </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
