7dd154197c9385fa1482083b7d506cdce0537774
juandreww <juandreww@outlook.com>
2022-06-05 10:00:08 +0700

    report

--- resources/views/usingmoney/report.blade.php
+++ resources/views/usingmoney/report.blade.php
@@ -7,11 +7,12 @@
     <meta name="_token" content="{{csrf_token()}}" />
     <title>Report</title>
     <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
         body {
             margin: 0;
             font-family: "Poppins", sans-serif;
         }
+
         .sidenav .sidenav-button {
             font-size: 32px;
 
