7106badce5797609663e40e3a83ccb23471e4b94
juandreww <juandreww@outlook.com>
2022-06-08 22:24:46 +0700

    my list 2

--- resources/views/usingmoney/mylist2.blade.php
+++ resources/views/usingmoney/mylist2.blade.php
@@ -7,6 +7,14 @@
     <title>List - Try 2</title>
 </head>
 <body>
+    <div class="sidenav"></div>
+    <div class="content">
+        <div class="content-header"></div>
+        <div class="content-body">Uta Here!!</div>
+    </div>
 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
 </body>
 </html>
