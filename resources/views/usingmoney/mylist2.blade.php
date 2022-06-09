58fd6ad4c51622f6236e3afae2ef3fcfcd7abf22
juandreww <juandreww@outlook.com>
2022-06-10 06:42:19 +0700

    sss

--- resources/views/usingmoney/mylist2.blade.php
+++ resources/views/usingmoney/mylist2.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="_token" content="{{csrf_token()}}" />
     <title>List - Try 2</title>
     <style>
