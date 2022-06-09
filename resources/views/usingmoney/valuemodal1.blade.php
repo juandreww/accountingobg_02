242d29b3768b0bf8f4107d56db1e70547338830b
juandreww <juandreww@outlook.com>
2022-06-09 23:26:10 +0700

    valuemodal1

--- resources/views/usingmoney/valuemodal1.blade.php
+++ resources/views/usingmoney/valuemodal1.blade.php
@@ -5,8 +5,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Value Modal</title>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
 </head>
 <body>
+    <div class="container mt-2">
+        Name <input type="text" name="name" id="name"><br><br>
+        Marks <input type="text" name="marks" id="marks"><br><br>
+        <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#exampleModal" id="submit">
+            Submit
+        </button>
 
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">
+                        Confirmation
+                    </h5>
+
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">
+                            x
+                        </span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <h6 id="modal_body"></h6>
+                    <button class="btn btn-success btn-sm" id="submit" type="button" data-toggle="modal" data-target="#exampleModal">
+                        Submit
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script></script>
 </body>
 </html>
