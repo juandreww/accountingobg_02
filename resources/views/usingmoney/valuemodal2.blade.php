7df2a430a436bb376a01a68073e20d28fd7a20f1
juandreww <juandreww@outlook.com>
2022-06-11 23:36:32 +0700

    valuemodal2

--- resources/views/usingmoney/valuemodal2.blade.php
+++ resources/views/usingmoney/valuemodal2.blade.php
@@ -21,6 +21,23 @@
         .container button {
             width: 270px;
         }
+
+        table tr td {
+            padding: 10px;
+        }
+
+        table {
+            width: 100%;
+        }
+
+        #firstcol {
+            width: 5%;
+        }
+
+        #secondcol {
+            width: 95%;
+        }
+
     </style>
     {{-- CSS --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -32,11 +49,47 @@
 </head>
 <body>
     <div class="container">
-        Name &emsp;<input type="text" name="name" id="name"><br><br>
-        Note &emsp;<input type="text" name="note" id="note"><br><br>
-        <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#exampleModal" id="submit">
-            Submit
-        </button>
+        <table>
+            <tr>
+                <td id="firstcol">Name</td>
+                <td id="secondcol"><input type="text" name="name" id="name"></td>
+            </tr>
+            <tr>
+                <td id="firstcol">Note</td>
+                <td id="secondcol"><input type="text" name="note" id="note"></td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#exampleModal" id="submit">
+                        Submit
+                    </button>
+                </td>
+            </tr>
+        </table>
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">
+                            Message
+                        </h5>
+
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">
+                                x
+                            </span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <h6 id="modal-body"></h6>
+                        <button class="btn btn-success btn-sm" id="submit" type="button" data-toggle="modal" data-target="#exampleModal">
+                            Submit
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
     <script type="text/javascript">
