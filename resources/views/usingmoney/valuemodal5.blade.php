597c4aba86022be554d81a3118b06ce956e0754c
juandreww <juandreww@outlook.com>
2022-06-12 22:34:41 +0700

    konijiwa

---
+++ resources/views/usingmoney/valuemodal5.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="_token" content="{{csrf_token()}}" />
+    <title>Value Modal 5</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
+
+        body {
+            margin: 0;
+            font-family: "Poppins", sans-serif;
+        }
+
+        .container {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            z-index: 1;
+        }
+
+        th {
+            background-color: #eeff41 !important;
+        }
+
+
+        tr:nth-child(odd) {
+            background-color: #eeeeee;
+        }
+    </style>
+    {{-- CSS --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    {{-- JS --}}
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <table class="table table-bordered">
+                    <tr>
+                        <th style="width: 250px">DATE</th>
+                        <th style="width: 350px">DESCRIPTION</th>
+                        <th style="width: 250px">AMOUNT</th>
+                    </tr>
+                    @foreach ($data as $row)
+                        @php
+                            $url = '/usingmoney/list?id=' . $d->uid;
+                        @endphp
+                        <a href= {{$url}} class="edit-modal-dialog stretched-link" data-toggle="modal" data-target="#exampleModal" id ="submit">
+                            <tr>
+                                <td>{{$row->date}}</td>
+                                <td>{{$row->note}}</td>
+                                <td>{{number_format($row->amount,2)}}</td>
+                            </tr>
+                        </a>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+
+
+    </div>
+    <script type="text/javascript">
+
+    </script>
+</body>
+</html>
