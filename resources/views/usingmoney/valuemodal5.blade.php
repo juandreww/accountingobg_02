99485ec3c11fcebc60e859020c0a91eac5178132
juandreww <juandreww@outlook.com>
2022-06-13 07:21:42 +0700

    valuemodal5

--- resources/views/usingmoney/valuemodal5.blade.php
+++ resources/views/usingmoney/valuemodal5.blade.php
@@ -30,6 +30,10 @@
         tr:nth-child(odd) {
             background-color: #eeeeee;
         }
+
+        table {
+            width: 100%;
+        }
     </style>
     {{-- CSS --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -51,7 +55,7 @@
                     </tr>
                     @foreach ($data as $row)
                         @php
-                            $url = '/usingmoney/list?id=' . $d->uid;
+                            $url = '/usingmoney/list?id=' . $row->uid;
                         @endphp
                         <a href= {{$url}} class="edit-modal-dialog stretched-link" data-toggle="modal" data-target="#exampleModal" id ="submit">
                             <tr>
@@ -66,6 +70,41 @@
         </div>
 
 
+    </div>
+
+    <div class="modal fade" id="exampleModal" tab-index="-1" aria-labelledby="examplemodalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="examplemodalLabel">Edit Transaction</h5>
+                    <button class="xclose" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <table>
+                            <div class="form-group">
+                                <label for="date" class="col-form-label"></label>
+                                <input type="date" class="form-control" id="dateinput">
+                            </div>
+                            <div class="form-group">
+                                <label for="amount" class="col-form-label">Amount</label>
+                                <input type="text" class="form-control" id="amount" name="amount" value="">
+                            </div>
+                            <div class="form-group">
+                                <label for="note" class="col-form-label">Note</label>
+                                <input type="text" class="form-control" id="note" name="note"value="">
+                            </div>
+                        </table>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button class="btn btn-primary" name="submit2" value="Submit" id="submit2"></button>
+                </div>
+            </div>
+        </div>
     </div>
     <script type="text/javascript">
 
