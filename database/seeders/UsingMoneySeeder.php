c37b66dc83b99dbd16af69e9c03e334573d73c4e
juandreww <juandreww@outlook.com>
2022-06-15 22:57:05 +0700

    faker usingmoney

--- database/seeders/UsingMoneySeeder.php
+++ database/seeders/UsingMoneySeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use \App\Models\UsingMoney;
+use Illuminate\Support\Str;
 
 class UsingMoneySeeder extends Seeder
 {
