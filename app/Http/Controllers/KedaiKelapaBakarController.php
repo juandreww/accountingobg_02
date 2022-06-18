ca44c6e4698ad2cd610900b0548e8c760a2cacaa
juandreww <juandreww@outlook.com>
2022-06-18 14:38:17 +0700

    kelapa bakar

---
+++ app/Http/Controllers/KedaiKelapaBakarController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use \Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use \App\Models\User;
+use \App\Models\UsingMoney;
+use Illuminate\Support\Str;
+use Maatwebsite\Excel\Facades\Excel;
+
+class KedaiKelapaBakarCOntroller extends Controller
+{
+    public function list(Request $request) {
+
+    }
+
+    public function config(Request $request) {
+
+    }
+
+    public function save(Request $request) {
+
+    }
+
+    public function delete(Request $request) {
+
+    }
+
+    public function upload(Request $request) {
+
+    }
+
+    public function deletefile(Request $request) {
+
+    }
+}
