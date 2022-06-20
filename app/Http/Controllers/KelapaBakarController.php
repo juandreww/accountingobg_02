d3ade63ba8c0642caf9ec9b22dc8ff729e2c7289
juandreww <juandreww@outlook.com>
2022-06-20 07:15:26 +0700

    seeding factory

--- app/Http/Controllers/KelapaBakarController.php
+++ app/Http/Controllers/KelapaBakarController.php
@@ -18,7 +18,9 @@ class KelapaBakarController extends Controller
     }
 
     public function show(Request $request) {
+        $faker = Faker::create('id_ID');
 
+        dd($date);
     }
 
     public function save(Request $request) {
