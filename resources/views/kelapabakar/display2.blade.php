9947396e0f536a9508fee833d0aa0700a4724267
juandreww <juandreww@outlook.com>
2022-07-02 13:23:41 +0700

    body sidenav

--- resources/views/kelapabakar/display2.blade.php
+++ resources/views/kelapabakar/display2.blade.php
@@ -5,8 +5,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Display: 2 Kanban</title>
+    <style>
+
+        body {
+            margin: 0;
+            /* background-color: #e4e4e4 !important; */
+        }
+        .pg_header {
+            height: 40px;
+            width: 100%;
+            position: fixed;
+            z-index: 1;
+            top: 0;
+            left: 0;
+            /* border-bottom: 2px solid #d9d9d9; */
+            background-color: white;
+            box-shadow: 0px 0.1px 3px #888888;
+        }
+
+        .pg_body {
+            position: fixed;
+            top: 40px;
+            background-color: #fafbfd;
+            display: inline-block;
+            height: 100%;
+            width: 150px;
+        }
+
+        .ct_header {
+            /* position: fixed; */
+            top: 40px;
+            /* left: 150px; */
+        }
+
+        .ct_body {
+            /* position: fixed; */
+            top: 60px;
+            /* left: 150px; */
+        }
+    </style>
 </head>
 <body>
-    
+    <div class="pg_header">This is Page Header</div>
+    <div class="pg_body">
+        <div class="pg_sidenav"></div>
+        <div class="ct_header">This is Content Header</div>
+        <div class="ct_body">This is Content Body</div>
+    </div>
+    {{-- <div class="ct_header">This is Content Header</div>
+    <div class="ct_body">
+        This is Content Body
+    </div> --}}
 </body>
 </html>
