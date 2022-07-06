671737353d6d16f17f921151e5dcf010270e10fd
juandreww <juandreww@outlook.com>
2022-07-06 07:04:51 +0700

    javascript

--- resources/views/kelapabakar/display3.blade.php
+++ resources/views/kelapabakar/display3.blade.php
@@ -361,5 +361,12 @@
             <a href="#"><span class="material-symbols-outlined">add</span></a>
         </div>
     </div>
+
+    {{-- JS --}}
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+
+    <script type="text/javascript"></script>
 </body>
 </html>
